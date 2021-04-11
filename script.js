f3f97fa607469b984649950e4ecb47b0e5fdb179
Dmitry <80142091+ChustaD@users.noreply.github.com>
2021-04-11 21:38:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ if (isMobile.any()) {
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
-    iconMenu.addEventListener('click', function (e) {
+    iconMenu.addEventListener('click', function () {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
